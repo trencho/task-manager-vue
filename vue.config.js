2c1232f26a0a02938ac8922ada81f848d78e6563
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-02-05 21:26:03 +0100

    Updated dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
 module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      })
+    ],
+  },
   devServer: {
     proxy: {
       '/api': {
